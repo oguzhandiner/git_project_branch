ff1159177d1e55378825af78caf6f13dc878c8c1
Oguzhan <oguzhandiner061@gmail.com>
2020-08-16 19:38:48 +0300

    featureden ilk commit

--- src/BranchDeneme.java
+++ src/BranchDeneme.java
@@ -5,6 +5,9 @@ public class BranchDeneme {
 		
 		System.out.println("saat 19:28 Master Branch'den ilk commit");
 		
+		
+		System.out.println("saat 19:37 feature Brach'ten ilk komut");
+		
 	}
 
 }
